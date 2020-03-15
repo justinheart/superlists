873e2170124a6313c5a8f02f6d73a3696f6427ea
Justin Chang <justin2018777@gmail.com>
2020-03-15 19:23:24 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,26 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
 class HomePageTest(TestCase):
 
@@ -10,7 +30,7 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    '''
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
@@ -19,7 +39,7 @@ class HomePageTest(TestCase):
         #self.assertTrue(response.content.strip().endswith(b'</html>'))
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
-    '''
+
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
