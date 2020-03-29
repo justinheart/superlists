c7e44f50d0eb89d26fe7bb373bac04ff93643739
Justin Chang <justin2018777@gmail.com>
2020-03-29 16:06:51 +0800

    add new functional test for lists usage with different users

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -34,16 +34,9 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(5)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/list/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(
-        #    any(row.text == '1: Buy peacock feathers' for row in rows),
-        #    "New to-do item did not appear in table -- its text was:\n%s" % (table.text),
-        #)
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
@@ -51,6 +44,27 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(5)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Buy milk', page_text)
 
         self.fail('Finish the test!')
 
