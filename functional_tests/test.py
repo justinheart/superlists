6638ced84131fbadb1998ace9f838d56e4aa2f5b
Justin Chang <justin2018777@gmail.com>
2020-03-29 19:29:43 +0800

    new URL + view for adding to existing lists. FT passes.

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -41,7 +41,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(3)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
@@ -56,7 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(3)
 
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
@@ -64,9 +64,9 @@ class NewVisitorTest(LiveServerTestCase):
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('Buy milk', page_text)
+        self.assertIn('Buy milk', page_text)
 
-        self.fail('Finish the test!')
+        #self.fail('Finish the test!')
 
 
 #if __name__ == '__main__':
